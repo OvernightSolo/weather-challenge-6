5013dc3deafb38a89ce377ca311051ff830d7b7e
OvernightSolo <sterlingtx75@gmail.com>
2022-07-31 16:13:33 -0500

    got current weather to populate

--- assets/script.js
+++ assets/script.js
@@ -5,52 +5,65 @@ var cityNameEl = $("#city");
 var temperatureEl = $("#temp");
 var windEl = $("#wind");
 var humidityEl = $("#humidity");
-var descriptionEl = $("#description");
-var today = moment().format("MM/DD/YYYY");
-var forecastBody = $(".main-forecast");
-var cityEntered = cityInputEl.val();
+var uvIndexEl = $("#uv-index");
+var key = "620e0c436745e0d32b6d06368bb0fd5f";
+var cityEntered;
+var forecastBody = $(".current");
 
 function handleFormSubmit(e) {
   e.preventDefault();
-
+  var cityEntered = cityInputEl.val();
   if (!cityEntered) {
     alert("Please enter a city.");
     return;
   }
   cityHistoryEl.append("<button>" + cityEntered + "</button>");
-
   cityInputEl.val("");
-}
+  var today = moment().format("MM/DD/YYYY");
+  var coords =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    cityEntered +
+    "&appid=" +
+    key;
 
-function getWeather() {
-  let key = "620e0c436745e0d32b6d06368bb0fd5f";
-  let language = "en";
-  let units = "imperial";
-  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?cityEntered=${cityEntered}&appid=${key}&units=${units}&language=${language}`;
-
-  fetch(requestUrl)
+  fetch(coords)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      var nameValue = data.name + " (" + today + ")";
-      var tempValue = "Temperature: " + data.main.temp + " °F";
-      var windValue = "Wind Speed: " + data.wind.speed + " mph";
-      var humidityValue = "Humidity: " + data.main.humidity + "%";
-      var descriptionValue = "Conditions: " + data.weather[0].description;
-      var iconCode = data.weather[0].icon;
+      return fetch(
+        "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+          data[0].lat +
+          "&lon=" +
+          data[0].lon +
+          "&exclude=hourly,minutely&units=imperial&appid=" +
+          key
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          var nameValue = cityEntered + " " + today;
+          var tempValue = "Temperature: " + data.current.temp + " °F";
+          var windValue = "Wind Speed: " + data.current.wind_speed + " mph";
+          var humidityValue = "Humidity: " + data.current.humidity + "%";
+          var uvIndexValue = "UV Index: " + data.current.uvi;
 
-      $(cityNameEl).html(nameValue);
-      $(temperatureEl).html(tempValue);
-      $(windEl).html(windValue);
-      $(humidityEl).html(humidityValue);
-      $(descriptionEl).html(descriptionValue);
+          $(cityNameEl).html(nameValue);
+          $(temperatureEl).html(tempValue);
+          $(windEl).html(windValue);
+          $(humidityEl).html(humidityValue);
+          $(uvIndexEl).html(uvIndexValue);
+
+          $(forecastBody).append();
+        });
     });
-
-  $(forecastBody).append();
 }
 
-getWeather();
+// function getWeather() {}
+
+// getWeather();
 
 searchButtonEl.on("click", handleFormSubmit);
